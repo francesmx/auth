b69e5ebb95064b85c4244f057f5008d1e6369710
Frances Maxwell <fmaxwell@gmail.com>
2017-03-03 23:32:54 +0000

    Added linter, config.js and new LoginForm component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import FIREBASE_API_KEY from '../config';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: FIREBASE_API_KEY,
+      authDomain: 'auth-21011.firebaseapp.com',
+      databaseURL: 'https://auth-21011.firebaseio.com',
+      storageBucket: 'auth-21011.appspot.com',
+      messagingSenderId: '293141528103'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   }
