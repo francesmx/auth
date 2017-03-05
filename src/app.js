1968fd9c9290f4b70f5cad85e5c60e0720a97d63
Frances Maxwell <fmaxwell@gmail.com>
2017-03-05 00:21:35 +0000

    Added spinner component and renderContent based on loggedIn status

--- src/app.js
+++ src/app.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import FIREBASE_API_KEY from '../config';
-import { Header } from './components/common';
+import { CardSection, Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: FIREBASE_API_KEY,
+      apiKey: '',
       authDomain: 'auth-21011.firebaseapp.com',
       databaseURL: 'https://auth-21011.firebaseio.com',
       storageBucket: 'auth-21011.appspot.com',
       messagingSenderId: '293141528103'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        <CardSection>
+          {this.renderContent()}
+        </CardSection>
       </View>
     );
   }
